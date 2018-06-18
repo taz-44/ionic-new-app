ee0a78b58ebb284746862d149a00545bce5b2cde
Tyson <tyson.seow@gmail.com>
2018-06-18 09:34:05 -0600

    ionic first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {FooPageModule} from "../pages/foo/foo.module";
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    FooPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
